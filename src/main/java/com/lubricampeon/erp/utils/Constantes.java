5f5f721d86dc12406d90a1406c8b4499ac2f7338
Fernando Vazquez <fernandorafa@live.com>
2023-05-27 13:42:21 -0600

    - adaptacion de constantes para contenedores

--- src/main/java/com/lubricampeon/erp/utils/Constantes.java
+++ src/main/java/com/lubricampeon/erp/utils/Constantes.java
@@ -2,10 +2,15 @@ package com.lubricampeon.erp.utils;
 
 public final class Constantes {
 
+    //TODO: esta variale se usa para contenedores docker
+    public static final String JASPER_FILES_PATH_DOCKER = "/app/jasper";
+
     public static final String SEPARADOR_WINDOWS = "\\";
     public static final String CARPETA_WINDOWS = "C:\\Sistema\\imagenes";
     public static final String CARPETA_LINUX = "/home/Sistema/CapitalSys/";
-    public static final String CARPETA_REPORTES_WINDOWS = "D:\\Sistema\\reportes";
+    //TODO: si el servicio se ejecuta en local descomentar esta linea
+   // public static final String CARPETA_REPORTES_WINDOWS = "D:\\Sistema\\reportes";
+    public static final String CARPETA_REPORTES_WINDOWS = JASPER_FILES_PATH_DOCKER;//"/app/reportes";
     public static final String CARPETA_REPORTES_LINUX = "/home/Sistema/Informes";
 
 }
